7cce33e3116600b1f1243684b7e7bba510114e98
Somesh Dwivedi <sd10072004@gmail.com>
2024-10-13 23:16:02 +0530

    deployed on local network

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,8 +6,61 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
-async function main() {
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether')
+}
 
+async function main() {
+  [buyer,seller,inspector,lender] = await ethers.getSigners();  
+
+  //Deploy real estate  
+  const RealEstate = await ethers.getContractFactory('RealEstate');  
+  const realEstate = await RealEstate.deploy();  
+  await realEstate.deployed();
+  
+  console.log(`Deployed Real Estate Contract at: ${realEstate.address}`)
+  console.log(`Minting 3 properties...\n`)
+  
+  const jsonFiles = [
+    "https://gateway.pinata.cloud/ipfs/QmQJc3tWrenPYqqHHWFVTTNxBww3Zagyr2udhPGCYn6mze" , 
+    "https://gateway.pinata.cloud/ipfs/QmcpphVWNwi5BvKS5ztxjHVwTmz1ueJPTYKVjghLF1RE6b" , 
+    "https://gateway.pinata.cloud/ipfs/QmS9iosc3ux7vC2D7ijWHhvW3LsjoEnMbcfLpDCiqQoZWf" ] ;  
+  for (let i = 0; i < 3; i++) {
+    const transaction = await realEstate.connect(seller).mint(jsonFiles[i])
+    await transaction.wait()
+  }
+
+  //Deploy Escrow  
+  const Escrow = await ethers.getContractFactory('Escrow');
+  const escrow = await Escrow.deploy(
+    realEstate.address, 
+    seller.address, 
+    inspector.address,
+    lender.address 
+  )
+  await escrow.deployed() ;  
+
+  console.log(`Deployed Escrow Contract at: ${escrow.address}\n`); 
+  console.log(`Listing 3 properties...\n`);
+
+  for (let i = 0; i < 3; i++) {
+    // Approve properties...
+    let transaction = await realEstate.connect(seller).approve(escrow.address, i + 1)
+    await transaction.wait()
+  }
+
+   // Listing properties...
+   transaction = await escrow.connect(seller).list(1, buyer.address, tokens(20), tokens(10))
+   await transaction.wait()
+ 
+   transaction = await escrow.connect(seller).list(2, buyer.address, tokens(15), tokens(5))
+   await transaction.wait()
+ 
+   transaction = await escrow.connect(seller).list(3, buyer.address, tokens(10), tokens(5))
+   await transaction.wait()
+ 
+   console.log(`Finished.`)
+  
 }
 
 // We recommend this pattern to be able to use async/await everywhere
